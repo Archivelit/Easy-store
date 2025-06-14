79386261762007083589cdf3ba8f63622a5d353e
Archivelit <dalbaeb1488322666@gmail.com>
2025-06-14 20:33:37 +0200

    managed packages

--- src/Store.Infrastructure/Data/Properties/AssemblyInfo.cs
+++ src/Store.Infrastructure/Data/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("SqliteTests")]
-[assembly: InternalsVisibleTo("API")]
+[assembly: InternalsVisibleTo("tests")]
+[assembly: InternalsVisibleTo("Store.API")]
